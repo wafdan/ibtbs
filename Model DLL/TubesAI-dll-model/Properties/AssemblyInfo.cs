d8aba25a847caf4cec265429bff8f6031ca3776b
zulfikar.hakim@gmail.com <zulfikar.hakim@gmail.com@1e530bf0-1623-11df-b401-9b904dc24c40>
2010-02-11 09:31:06 +0000

    Project di sini sudah jadi buat dll-nya, tinggal di load aja. Good luck everyone. -Zulfikar Hakim-

--- Model DLL/TubesAI-dll-model/Properties/AssemblyInfo.cs
+++ Model DLL/TubesAI-dll-model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TubesAI")]
-[assembly: AssemblyProduct("TubesAI")]
+[assembly: AssemblyTitle("TubesAI-dll-model")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-
+[assembly: AssemblyProduct("TubesAI-dll-model")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,8 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8a89b760-94b4-4c4f-b6de-0e27b21ef66d")]
-
+[assembly: Guid("0fb30674-0b3c-45a7-a8e0-77278c812622")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
